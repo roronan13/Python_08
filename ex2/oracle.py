ff8ad360dd5a497e08b082538e5bb1e2fbd8e9ac
roronan13 <ronan.pothier@gmail.com>
2026-05-26 02:28:13 +0200

    dacc

--- ex2/oracle.py
+++ ex2/oracle.py
@@ -1,4 +1,27 @@
-if __name__ == "__main__":
-    print("  ORACLE STATUS : Reading the Matrix ...  ")
+import sys
+import os
 
-    
+if __name__ == "__main__":
+    print("  ORACLE STATUS : Reading the Matrix ...  \n")
+
+    try:
+        from dotenv import load_dotenv
+    except ImportError as e:
+        print(f"{e} : Can't import load_dotenv from dotenv.")
+        print("Program will exit. Try <pip install python-dotenv>.")
+        sys.exit()
+
+    load_dotenv()
+
+    mode: str = os.getenv("MATRIX_MODE")
+    database_url: str = os.getenv("DATABASE_URL")
+    api_key: str = os.getenv("API_KEY")
+    log_level: str = os.getenv("LOG_LEVEL")
+    zion_endpoint: str = os.getenv("ZION_ENDPOINT")
+
+    print("Configuration loaded : ")
+    print(f"Mode : {mode}")
+    print(f"Database : {database_url}")
+    print(f"API Access : {api_key}")
+    print(f"Log Level : {log_level}")
+    print(f"Zion Network : {zion_endpoint}")
